4200b36f32cfdf14a3b540d51c50b7074cd648a1
YEXIU21 <iamshadowobscurity11@gmail.com>
2026-02-22 22:09:33 +0800

    Web: Use HashRouter to prevent Vercel refresh 404s

--- devxflow-web/frontend/src/App.tsx
+++ devxflow-web/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { MainLayout } from './components/layout/MainLayout'
 import { AuthLayout } from './components/layout/AuthLayout'
@@ -47,7 +47,7 @@ const queryClient = new QueryClient()
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           {/* Public Routes */}
           <Route element={<MainLayout />}>
@@ -82,7 +82,7 @@ function App() {
           {/* 404 */}
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   )
 }
